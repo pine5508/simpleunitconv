b5c151bd90bde0d6d7f8d99b0f7d118ae6c6c8cd
pine5508 <joungwon04@gmail.com>
2020-05-08 22:41:03 +0900

    km -> miles

--- simpleunitconv.py
+++ simpleunitconv.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+# Import some libraries
+import tkinter
+
+# Some variables
+output = 1.0
+userInput = 1.0
+
+# Let's try making km -> mile first
+userInput = float(input('How much km? '))
+output = userInput*0.621
+print(str(userInput) + 'km is about ' + str(output) + ' miles.')
+
+"""
+Tomorrow I will try to make more features, and combine them as different functions
+"""
