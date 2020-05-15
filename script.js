a0e56e7ef98f4aaa78ee48151e11e35af3d311a2
pine5508 <joungwon04@gmail.com>
2020-05-16 02:53:07 +0900

    Get user input

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-const func = (e) => {
-    console.log(e)
+function calculate() {
+    var n = document.getElementById("input").value;
+    var u = document.getElementById("unit");
+    var strUser = u.options[u.selectedIndex].value;
+
+    console.log(n);
+    console.log(strUser);
 }
