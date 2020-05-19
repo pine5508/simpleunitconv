ef439c28ca889e0e641bf34aa985c9f77e54c086
pine5508 <joungwon04@gmail.com>
2020-05-20 03:59:32 +0900

    Changed functions and units

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 function calculate() {
-    var km = document.getElementById("input").value
+    var input = document.getElementById("input").value
     var u = document.getElementById("unit")
     var strUser = u.options[u.selectedIndex].value
     var output = 0
 
-    if (strUser == "meters") { output = km * 1000 }
-    else if (strUser == "centimeters") { output = km * 100000 }
-    else if (strUser == "miles") { output = km * 0.621371 }
-    else if (strUser == "yards") { output = km * 1093.61 }
-    else if (strUser == "feet") { output = km * 3280.84 }
+    // to be added
 
     document.getElementById("result").innerHTML = km+" km is "+output+" "+strUser+"."
+}
+
+// kilometer conversions
+function kmTOmi() {
+
+}
+function kmTOft() {
+    
+}
+
+// mile conversions
+function miTOkm() {
+    
+}
+function miTOft() {
+    
+}
+
+// feet conversions
+function ftTOkm() {
+    
+}
+function ftTOmi() {
+    
 }
