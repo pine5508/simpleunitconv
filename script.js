4730befb1230a5efa46d22c3ac395966f70a1fc7
pine5508 <joungwon04@gmail.com>
2020-05-21 01:57:50 +0900

    Complete app

--- script.js
+++ script.js
@@ -1,34 +1,62 @@
+var input
+var u
+var unit
+var output
+
+function submit() {
+    input = document.getElementById("input").value
+    u = document.getElementById("unit")
+    unit = u.options[u.selectedIndex].value
+    calculate()
+}
+
 function calculate() {
-    var input = document.getElementById("input").value
-    var u = document.getElementById("unit")
-    var strUser = u.options[u.selectedIndex].value
-    var output = 0
-
-    // to be added
-
-    document.getElementById("result").innerHTML = km+" km is "+output+" "+strUser+"."
+    if (unit = "km") {
+        document.getElementById("kmOutput").value = input
+        kmTOmi()
+        kmTOft()
+    } else
+    if (unit = "mi") {
+        document.getElementById("miOutput").value = input
+        miTOkm()
+        miTOft()
+    } else
+    if (unit = "ft") {
+        document.getElementById("ftOutput").value = input
+        ftTOkm()
+        ftTOmi()
+    }
+    else {
+        alert("Invalid unit!")
+    }
 }
 
 // kilometer conversions
 function kmTOmi() {
-
+    output = input * 0.621371
+    document.getElementById("miOutput").value = output
 }
 function kmTOft() {
-    
+    output = input * 3280.84
+    document.getElementById("ftOutput").value = output
 }
 
 // mile conversions
 function miTOkm() {
-    
+    output = input * 1.6
+    document.getElementById("kmOutput").value = output
 }
 function miTOft() {
-    
+    output = input * 5280
+    document.getElementById("ftOutput").value = output
 }
 
 // feet conversions
 function ftTOkm() {
-    
+    output = input * 0.0003048
+    document.getElementById("kmOutput").value = output
 }
 function ftTOmi() {
-    
+    output = input * 0.000189
+    document.getElementById("miOutput").value = output
 }
